71065085c54074786007883e53877a5777dafabd
jaeyoung <young042510@gmail.com>
2025-01-18 15:27:36 +0900

    commit

---
+++ MealShop/src/test/java/com/mealShop/controller/MealShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealShop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
