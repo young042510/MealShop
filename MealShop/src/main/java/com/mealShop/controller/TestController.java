1ce4d098c40da209d770610251b54578ee386c9c
jaeyoung <young042510@gmail.com>
2025-01-18 15:54:43 +0900

    test

---
+++ MealShop/src/main/java/com/mealShop/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.mealShop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("/")
+	public String main() {
+		System.out.println("성공");
+		return "test";
+	}
+}
