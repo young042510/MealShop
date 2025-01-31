e79a669f84ad55f7ba056eb9d4d92f5547e353e3
hy <hy@DESKTOP-VC360JJ>
2025-01-31 23:21:17 +0900

    회원 리뷰 기능 구현 본인이 작성한 리뷰 목록 조회, 리뷰 삭제, 리뷰 작성 폼, 리뷰 작성 후 제출 주문 상품 건수(상품갯수X)에 따라서 리뷰 작성이 가능하도록 횟수 수정

--- MealShop/src/main/java/com/mealShop/service/MReviewService.java
+++ MealShop/src/main/java/com/mealShop/service/MReviewService.java
@@ -21,6 +21,34 @@ public class MReviewService {
 	public void getproductorderList(HashMap<String, Object> paramMap) {
 		reviewDao.getproductorderList(paramMap);
 	}
+
+	public void listReview(HashMap<String, Object> paramMap) {
+		reviewDao.listReview(paramMap);
+	}
+
+	public void reviewDelete(HashMap<String, Object> paramMap) {
+		reviewDao.reviewDelete(paramMap);
+	}
+
+	public void insertReview(HashMap<String, Object> paramMap) {
+		reviewDao.insertReview(paramMap);
+	}
+
+	public int getUserOrderCnt(String userId, int pseq) {
+		HashMap<String, Object> paramMap = new HashMap<>();
+		paramMap.put("userId", userId);
+		paramMap.put("pseq", pseq);
+		
+		return reviewDao.getUserOrderCnt(paramMap);
+	}
+
+	public int getUserReviewCnt(String userId, int pseq) {
+		HashMap<String, Object> paramMap = new HashMap<>();
+		paramMap.put("userId", userId);
+		paramMap.put("pseq", pseq);
+		
+		return reviewDao.getUserReviewCnt(paramMap);
+	}
 	
 	
 }
