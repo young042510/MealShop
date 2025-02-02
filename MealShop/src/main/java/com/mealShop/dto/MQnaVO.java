e41be7f930ad1c35cf5ef308aa82c9bca4367b39
jaeyoung <young042510@gmail.com>
2025-02-03 03:40:32 +0900

    상품문의글

--- MealShop/src/main/java/com/mealShop/dto/MQnaVO.java
+++ MealShop/src/main/java/com/mealShop/dto/MQnaVO.java
@@ -3,6 +3,9 @@ package com.mealShop.dto;
 
 import java.sql.Timestamp;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
