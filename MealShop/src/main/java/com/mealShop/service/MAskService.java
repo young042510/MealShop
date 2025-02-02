edc67642e6cfe0421e99bbd25372ad7a857dded8
jaeyoung <young042510@gmail.com>
2025-02-03 01:40:15 +0900

    응답 수정

--- MealShop/src/main/java/com/mealShop/service/MAskService.java
+++ MealShop/src/main/java/com/mealShop/service/MAskService.java
@@ -4,12 +4,30 @@ import org.springframework.stereotype.Service;
 
 import com.mealShop.dao.IMAskDao;
 
-import lombok.AllArgsConstructor;
 
 @Service
-@AllArgsConstructor
 public class MAskService {
 
-	IMAskDao imAskDao;
+	@Autowired
+	IMAskDao madao;
 
+	public void listMAsk(HashMap<String, Object> paramMap) {
+		madao.listMask(paramMap);
+	}
+
+	public void getAsk(HashMap<String, Object> paramMap) {
+	madao.getAsk(paramMap);
+	}
+
+	public void getAllCountAsk(HashMap<String, Object> paramMap) {
+	 madao.getAllCountAsk(paramMap);
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public void insertAsk(HashMap<String, Object> paramMap) {
+		madao.insertAsk(paramMap);
+	}
+	
+	
+	
 }
