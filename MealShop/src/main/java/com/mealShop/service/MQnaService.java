024e640edf6c138bab9b39c01d91d854bbe2577b
jaeyoung <young042510@gmail.com>
2025-02-03 14:41:38 +0900

    상품문의 조회 응답

--- MealShop/src/main/java/com/mealShop/service/MQnaService.java
+++ MealShop/src/main/java/com/mealShop/service/MQnaService.java
@@ -2,7 +2,9 @@ package com.mealShop.service;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mealShop.dao.IMQnaDao;
 
