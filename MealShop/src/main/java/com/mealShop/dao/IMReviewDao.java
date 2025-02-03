08868468776cf2371ae81bb89bcd442559aea827
jaeyoung <young042510@gmail.com>
2025-02-03 14:43:28 +0900

    리뷰DAO 조회/등록/작성

--- MealShop/src/main/java/com/mealShop/dao/IMReviewDao.java
+++ MealShop/src/main/java/com/mealShop/dao/IMReviewDao.java
@@ -7,15 +7,18 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IMReviewDao {
 
-	void getReviewListByPseq(HashMap<String, Object> paramMap);
+   void getReviewListByPseq(HashMap<String, Object> paramMap);
 
-	void listReview(HashMap<String, Object> paramMap);
+   void getproductorderList(HashMap<String, Object> paramMap);
 
-	void productorderList(HashMap<String, Object> paramMap);
+   void listReview(HashMap<String, Object> paramMap);
 
-	void insertReview(HashMap<String, Object> paramMap);
+   void reviewDelete(HashMap<String, Object> paramMap);
 
-	void reviewDelete(HashMap<String, Object> paramMap);
+   void insertReview(HashMap<String, Object> paramMap);
 
+   int getUserOrderCnt(HashMap<String, Object> paramMap);
+
+   int getUserReviewCnt(HashMap<String, Object> paramMap);
 
 }
